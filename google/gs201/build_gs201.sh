7249ea73495f1383adf8f70d922b98f4e73b84da
Nicholas Lim <niclimcy@lineageos.org>
2026-05-16 23:03:33 +0800

    gs201: Download GKI prebuilts
    Change-Id: I8924e541736261b5eb8c3907b4aed65b175b576c

--- google/gs201/build_gs201.sh
+++ google/gs201/build_gs201.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0-only
 
+python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 15260253
+
 exec tools/bazel run \
     --config=stamp \
     --config=gs201 \
