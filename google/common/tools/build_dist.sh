7e0c3f0ba0dde5f74b6cbe0526d7727cc9d80785
Michael Bestas <mkbestas@gmail.com>
2026-07-13 05:50:08 +0300

    common: Download android15-6.6-2026-01_r37 GKI
    Change-Id: I5b021c5b33042d4fdd2f6984d25dc63f19ea41e8

--- google/common/tools/build_dist.sh
+++ google/common/tools/build_dist.sh
@@ -16,8 +16,8 @@ EOF
   exit 1
 fi
 
-python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 15511674
-python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 15511674
+python3 download_prebuilts.py --force --build_target kernel_aarch64 --build_id 15863337
+python3 download_prebuilts.py --force --build_target kernel_aarch64_16k --build_id 15863337
 
 exec "${WORKSPACE_DIR}/tools/bazel" run \
   --config="${DEVICE}" \
